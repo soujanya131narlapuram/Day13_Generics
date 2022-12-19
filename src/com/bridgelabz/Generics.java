a01af63112be0abb48833a1bc720e26321ce176c
soujanya131narlapuram <soujanya131narlapuram@gmail.com>
2022-12-19 22:09:11 +0530

    "uc3 commit"

--- src/com/bridgelabz/Generics.java
+++ src/com/bridgelabz/Generics.java
@@ -1,6 +1,22 @@
 package com.bridgelabz;
 
 public class Generics {
+
+    static void max_variables(String str1, String str2, String str3){
+        if(str1.compareTo(str2) > 0){
+            if(str1.compareTo(str3) > 0){
+                System.out.println( str1 + " is Maximum of three strings ");
+            }else {
+                System.out.println( str3 + " is Maximum of three strings ");
+            }
+        }else{
+            if(str2.compareTo(str3) > 0){
+                System.out.println( str2 + " is Maximum of three strings ");
+            }else{
+                System.out.println( str3 + " is Maximum of three strings ");
+            }
+        }
+    }
     static void max_variables(Integer num1, Integer num2, Integer num3){
         if(num1.compareTo(num2) > 0){
             if(num1.compareTo(num3) > 0){
@@ -36,5 +52,6 @@ public class Generics {
     public static void main(String[] args) {
         max_variables(10,500,99);
         max_variables(6.1f,1.8f,70f);
+        max_variables("Orange", "WaterMelon", "Banana");
     }
 }
