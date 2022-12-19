71d89c186ecdd6144465b947f426863cb399ee8a
soujanya131narlapuram <soujanya131narlapuram@gmail.com>
2022-12-19 22:26:07 +0530

    "UC4 commit"

--- src/com/bridgelabz/Generics.java
+++ src/com/bridgelabz/Generics.java
@@ -1,39 +1,33 @@
 package com.bridgelabz;
 
 public class  Generics <T extends Comparable> {
-    T vble1;
-    T vble2;
-    T vble3;
+    T[] array;
 
-    Generics(T var1, T var2, T var3){   // parameterized constructor is used to initialize instance variable of objects
-        this.vble1 = var1;
-        this.vble2 = var2;
-        this.vble3 = var3;
+    Generics(T[] arr1) {
+        this.array = arr1;
     }
 
     void maximumOfVariables(){
-        if(this.vble1.compareTo(this.vble2) > 0){
-            if(this.vble1.compareTo(this.vble3) > 0){
-                System.out.println( this.vble1 + " is Maximum of three variables");
-            }else {
-                System.out.println( this.vble3 + " is Maximum of three variables ");
-            }
-        }else{
-            if(this.vble2.compareTo(this.vble3) > 0){
-                System.out.println( this.vble2 + " is Maximum of three variables ");
-            }else{
-                System.out.println( this.vble3 + " is Maximum of three variables ");
+        T largest = this.array[0];
+        for (int i=0; i<this.array.length; i++){
+            if(this.array[i].compareTo(largest) > 0){
+                largest = this.array[i];
+                this.array[i] = largest;
             }
         }
+        System.out.println(" Largest element is " + largest);
     }
+
     public static void main(String[] args) {
-        Generics <Integer> obj1 = new Generics(60, 1, 45);
+        Integer[] arr1 = {12,4,7,10,6};
+        Generics <Integer> obj1 = new Generics(arr1);
         obj1.maximumOfVariables();
 
-        Generics <String> obj2 = new Generics("Orange", "Banana","WaterMelon");
+        String[] arr2 = {"kavya", "jaya", "ganesh"};
+        Generics <String> obj2 = new Generics(arr2);
         obj2.maximumOfVariables();
 
-        Generics <Float> obj3 = new Generics(5.3f, 7.1f, 10.7f);
+        Float[] arr3 = {2.1f, 5.4f, 6.0f};
+        Generics <Float> obj3 = new Generics(arr3);
         obj3.maximumOfVariables();
-    }
-}
+    }}
